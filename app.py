afcabe129fc972f30ed7f5ec8a594ab026ebc161
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-07-12 01:54:27 +0900

    메인화면, 결과화면, 검색화면, 회원수정화면 프레임 생성완료

--- app.py
+++ app.py
@@ -9,7 +9,20 @@ def home():
 
 @app.route('/login')
 def getLogin():
-    return render_template('login.html');
+    return render_template('login.html')
+
+@app.route('/user')
+def getUser():
+    return render_template('user.html')
+
+@app.route('/register')
+def getRegister():
+    return render_template('register.html')
+
+@app.route('/search')
+def getSearch():
+    return render_template('search.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
