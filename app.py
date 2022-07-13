6708e308dfa9cb436eb837eaec736060b58383d0
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-07-13 09:51:30 +0900

    카카오 주소api 구현완료. 날씨페이지 구현완료

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ def getRegister():
 def getSearch():
     return render_template('search.html')
 
+@app.route('/weather')
+def getWeather():
+    return render_template('weather.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
